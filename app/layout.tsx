7a52a91bcda63972aa572af4057ab942c496632d
Saleh Hassan DASSOUKHI <sdassoukh@FRL3352.local>
2023-09-11 19:54:10 +0200

    scroll

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="manifest" href="/public/manifest.json" />
+      </head>
       <NextAuthProvider>
         <body>
           <Header />
